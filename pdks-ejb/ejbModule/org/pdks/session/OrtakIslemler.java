f5987d597845ca6f1284e7ffbbd3be7deca5ee04
pdksJava <javaabh@gmail.com>
2024-03-05 20:17:08 +0300

    .

--- pdks-ejb/ejbModule/org/pdks/session/OrtakIslemler.java
+++ pdks-ejb/ejbModule/org/pdks/session/OrtakIslemler.java
@@ -12315,7 +12315,9 @@ public class OrtakIslemler implements Serializable {
 							izinTipi = senelikIzinOlustur(veriMap, session);
 							if (gecmis && izinTipi != null) {
 								cal.setTime(bugun);
-								cal.add(Calendar.MONTH, -3);
+								cal.add(Calendar.MONTH, -2);
+								if (senelikKullan)
+									senelikKullan = suaDurum;
 								if (senelikKullan && buYil != cal.get(Calendar.YEAR)) {
 									senelikKullan = buYil > 2020 && buYil >= PdksUtil.getSistemBaslangicYili();
 									HashMap fields = new HashMap();
